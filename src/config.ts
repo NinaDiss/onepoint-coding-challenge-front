aa6013870b4a2d7b4aea6fd013e3216df0fd782c
Nina Dissard <dissardnina@gmail.com>
2026-02-12 16:40:43 +0100

    feat(test): add packages and config for the tests

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,9 @@
-// Environment configuration
 const config = {
-  // Backend API URL - can be overridden by environment variables
   backendApiUrl:
+    // Not in .env file for the coding challenge
     import.meta.env.VITE_BACKEND_API_URL ||
     "http://localhost:3000/breweries/parse",
 
-  // Open Brewery DB API URL
   openBreweryApiUrl: "https://api.openbrewerydb.org/v1/breweries",
 };
 
